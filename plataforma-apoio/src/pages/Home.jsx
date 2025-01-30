fc0791ac1b3faa38480f307e3a3949c9cd85af88
Jonata Monteiro <jonatamonteiro0306@gmail.com>
2025-01-30 19:36:52 -0300

    Estilizacao da header

--- plataforma-apoio/src/pages/Home.jsx
+++ plataforma-apoio/src/pages/Home.jsx
@@ -4,22 +4,21 @@ import "../styles/home.css"
 
 const Home = () => {
   return (
-    
+    <div className="home-wrapper">
       <div className="container">
-          <div className="header-home">
-            <img src="./src/assets/homeheader.jpg" alt="imagem header"/>
-            <div className="header-text">
-              <h2>Seu bem-estar é nossa prioridade, sempre.</h2>
-              <h1>Saúde mental acessível e inclusiva, porque você merece o melhor.</h1>
-              <p>Apoiamos sua jornada rumo ao equilíbrio emocional e ao sucesso, ajudando você a alcançar a melhor versão de si mesmo.</p>
-            </div>
-          </div>
-
-        <div className="busca">
-  
+        <div className="header-home">
+          <img src="./src/assets/homeheader.jpg" alt="imagem header" />
+        </div>
+        <div className="header-text">
+          <h2>Seu bem-estar é nossa prioridade, sempre.</h2>
+          <h1>Saúde mental acessível e inclusiva, porque você merece o melhor.</h1>
+          <p>Apoiamos sua jornada rumo ao equilíbrio emocional e ao sucesso, ajudando você a alcançar a melhor versão de si <spam>mesmo.</spam></p>
         </div>
       </div>
-
+      <div className="busca">
+          <Busca/>
+        </div>
+    </div>
   );
 };
 
