64f1b85b5b0583dff4c1388374c02d0d46eea60b
Joao.Wesley.Nas <joaowesleynascimentodacosta@gmail.com>
2025-02-12 16:30:31 -0300

    diminuindo tamanho

--- plataforma-apoio/src/pages/profissional.jsx
+++ plataforma-apoio/src/pages/profissional.jsx
@@ -3,28 +3,28 @@ import "../styles/profissional.css";
 const Profi = () => {
     return (
     <div className="body_prof">
-    <main class="separar_quad">
-        <section class="sep">
-            <div class="quadrado1">
-                <div class="prof">
-                    <div class="imagem_perfil"><p>imagem de perfil do usuário</p></div>
+    <main className="separar_quad">
+        <section className="sep">
+            <div className="quadrado1">
+                <div className="prof">
+                    <div className="imagem_perfil"><p>imagem de perfil do usuário</p></div>
                     <h1>Nome do profissional</h1>
                     <h3>Consultas realizadas:</h3>
-                    <div class="numero"><p>0</p></div>
+                    <div className="numero"><p>0</p></div>
                 </div>
 
             </div>
-            <div class="dados">
-                <div class="top_tit"><h1>Dados profissionais</h1></div>
-                <div class="info">
+            <div className="dados">
+                <div className="top_tit"><h1>Dados profissionais</h1></div>
+                <div className="info">
                     <div>
-                        <h1 class="nome">Dr. Lucas Oliveira Andrade</h1>
+                        <h1 className="nome">Dr. Lucas Oliveira Andrade</h1>
                     </div>
                     <h2 className="h2">CRM: CRP 01/XXXXX</h2>
                     <h2 className="h2">CPF: 087......07</h2>
                     <h2 className="h2">RG: 098......04</h2>
                     <h2 className="h2">Data de nascimento: 23/05/1995</h2>
-                    <button class="edit"><h1>Editar perfil</h1></button>
+                    <button className="edit"><h1>Editar perfil</h1></button>
                 </div>
             </div>
         </section>
@@ -34,7 +34,7 @@ const Profi = () => {
             <button><h1>Página inícial</h1></button>
             <button><h1>Histórico</h1></button>
         </nav>
-        <section class="apresent">
+        <section className="apresent">
             <h1>Sobre mim:</h1>
             <div>
                 <h2>Olá! Sou o Dr. Lucas Oliveira Andrade, psicólogo especializado em Psicoterapia Cognitivo-Comportamental. Com mais de 10 anos de experiência, ajudo pessoas a superarem desafios emocionais, promovendo bem-estar e qualidade de vida.
@@ -44,27 +44,27 @@ const Profi = () => {
             <div class="alinhamento">
                 <div>
                     <h1>Formação:</h1>
-                <div class="capsula"><h1>Psicologo</h1></div>
+                <div className="capsula"><h1>Psicologo</h1></div>
                 </div>
                 <div>
                     <h1>Abordagem terapêutica:</h1>
-                <div class="capsula"><h1>Psicoterapia Cognitivo-Comportamental</h1></div>
+                <div className="capsula"><h1>Psicoterapia Cognitivo-Comportamental</h1></div>
                 </div>
                 <h1>Especialidade:</h1>
-                <div class="aling">
-                    <div class="capsula"><h1> Ansiedade</h1></div><div class="capsula"><h1>Depressão</h1></div><div class="capsula"><h1>Transtorno de Ansiedade Generalizada (TAG)</h1></div><div class="capsula"><h1>Transtorno de Pânico</h1></div><div class="capsula"><h1>Fobia Social</h1></div><div class="capsula"><h1> Estresse Pós-Traumático</h1></div><div class="capsula"><h1>Trans. obsessivo-Compulsivo</h1></div>
+                <div className="aling">
+                    <div className="capsula"><h1> Ansiedade</h1></div><div className="capsula"><h1>Depressão</h1></div><div className="capsula"><h1>Transtorno de Ansiedade Generalizada (TAG)</h1></div><div className="capsula"><h1>Transtorno de Pânico</h1></div><div className="capsula"><h1>Fobia Social</h1></div><div className="capsula"><h1> Estresse Pós-Traumático</h1></div><div className="capsula"><h1>Trans. obsessivo-Compulsivo</h1></div>
                 </div>
                 <div>
                     <h1>Faixa etária:</h1>
-                    <div class="capsula"><h1>Adultos e Adolescentes</h1></div>
+                    <div className="capsula"><h1>Adultos e Adolescentes</h1></div>
                 </div>
                 <div>
                     <h1>Localidade de atuação:</h1>
-                <div class="capsula"><h1> Fortaleza, Ceará, Brasil</h1></div>
+                <div className="capsula"><h1> Fortaleza, Ceará, Brasil</h1></div>
                 </div>
                 <div>
                     <h1>tempo de atuação:</h1>
-                    <div class="capsula"><h1>10 anos</h1></div>
+                    <div className="capsula"><h1>10 anos</h1></div>
                 </div>
             </div>
         </section>
