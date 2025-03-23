eef5b8f9b241206899343af3834fc1303a5b3eed
Jonata Monteiro <jonatamonteiro0306@gmail.com>
2025-03-22 21:24:32 -0300

    ajuste nav

--- plataforma-apoio/src/components/Navbar.jsx
+++ plataforma-apoio/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import "../styles/Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
       <div className="container-navbar">
         <Link to="/" className="logo">
           Plataforma HDC
@@ -12,15 +13,14 @@ const Navbar = () => {
         <ul className="nav-links">
           <li><Link to="/">Início</Link></li>
           <li><Link to="/buscar">Buscar Profissionais</Link></li>
-          <Link to="/ajuda" className="btn-ajuda">Procurar Ajuda Agora</Link>
-          <li><Link to="/cadastroPaciente">Cadastro de Paciente</Link></li>
-          <li><Link to="/login">Login</Link></li>
           <li><Link to="/cadastroProfissional">Cadastro Profissional</Link></li>
 
         </ul>
-        <Link to="/ajuda" className="btn-ajuda">
-          Procurar Ajuda Agora
-        </Link>
+        <ul className="nav-link-login">
+          <Link to="/ajuda" className="btn-ajuda">Procurar Ajuda <i class='bx bx-search'></i></Link>
+          <Link to="/cadastroPaciente" className="signup">Criar conta</Link>
+          <Link to="/Login" className="btn-login">Entrar</Link>
+        </ul>
       </div>
     </nav>
   );
